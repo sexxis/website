a7c4e07dfeb69d87d7c1eaccbfadd2139fe5a871
Lawrence Pan <lawrencefeipan@gmail.com>
2016-06-03 17:46:31 -0400

    :sunglasses: gulp workflow

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var nodemon = require('gulp-nodemon');
+var browserSync = require('browser-sync').create();
+
+var PATHS = {
+  sass: "src/assets/styles/**/*.sass",
+  html: "src/**/*.html",
+};
+
+gulp.task('serve', ['nodemon', 'sass'], function() {
+
+    browserSync.init({
+      proxy: 'localhost:4200'
+    });
+
+    gulp.watch(PATHS.sass, ['sass']);
+    gulp.watch(PATHS.html).on('change', browserSync.reload);
+});
+
+gulp.task('sass', function() {
+   return gulp.src(PATHS.sass)
+        .pipe(sass())
+        .pipe(gulp.dest("dist"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('nodemon', function () {
+  nodemon({
+    script: './src/server.js'
+  });
+});
+
+gulp.task('default', ['serve']);
