6b36429bf06c4db168d82dbbba6720fb8f8605b2
Clive Chan <cc@clive.io>
2016-06-04 21:12:05 -0400

    added in linters

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var pug = require('gulp-pug');
-var nodemon = require('gulp-nodemon');
 var browserSync = require('browser-sync').create();
+var gulp = require('gulp');
+var eslint = require('gulp-eslint');
+var nodemon = require('gulp-nodemon');
+var pug = require('gulp-pug');
+var puglint = require('gulp-pug-lint');
+var sass = require('gulp-sass');
+var sasslint = require('gulp-sass-lint');
 
 var PATHS = {
+  eslint: "**/*.js",
   sass: "src/assets/styles/**/*.sass",
   pug: "src/views/*.pug",
 };
@@ -19,19 +23,35 @@ gulp.task('serve', ['nodemon', 'sass', 'pug'], function() {
 });
 
 gulp.task('build', ['sass', 'pug']);
-
 gulp.task('pug', function () {
   return gulp.src(PATHS.pug)
     .pipe(pug())
     .pipe(gulp.dest('dist'))
     .pipe(browserSync.stream());
 });
-
 gulp.task('sass', function() {
   return gulp.src(PATHS.sass)
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(gulp.dest('dist'))
     .pipe(browserSync.stream());
+});
+
+gulp.task('lint', ['eslint', 'puglint', 'sasslint']);
+gulp.task('eslint', function(){
+  return gulp.src(PATHS.eslint)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+gulp.task('puglint', function(){
+  return gulp.src(PATHS.pug)
+    .pipe(puglint());
+});
+gulp.task('sasslint', function(){
+  return gulp.src(PATHS.sass)
+    .pipe(sasslint())
+    .pipe(sasslint.format())
+    .pipe(sasslint.failOnError());
 });
 
 gulp.task('nodemon', function () {
