a74ace986b5ec4f9c128d92e17f74ffc31741a45
Lawrence Pan <lawrencefeipan@gmail.com>
2016-08-10 23:49:55 -0400

    remove express bootsrap route :zap:

--- src/server.js
+++ src/server.js
@@ -3,14 +3,10 @@ const path = require('path');
 
 const PORT = 4200;
 const PATH = path.join(__dirname, '..', 'dist');
-const BOOTSTRAP_PATH = path.join(
-    __dirname, '..', 'node_modules', 'bootstrap', 'dist'
-);
 
-var app = express();
+const app = express();
 
 app.use('/static', express.static(PATH));
-app.use('/bootstrap', express.static(BOOTSTRAP_PATH));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(PATH, 'index.html'));
